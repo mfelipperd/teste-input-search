2a22eeb206b9d9ab654c9e2b8b65231eab9c1ceb
mfelipperd <felipperabelodurans@gmail.com>
2025-07-02 20:28:58 -0300

    feat: adjust input to show imputName

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
+  import './+page.css';
+  import { goto } from '$app/navigation';
+  import type { PageData } from './$types';
+  export let data: PageData;
 
     let nameInput:  string = '';
     let debounceTimeout: ReturnType<typeof setTimeout>;
 
+  
     function onInput(): void {
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
@@ -31,4 +35,12 @@
     placeholder="ex: Ana"
     autocomplete="off"
   />
+   {#if data.age === null}
+    <p>Digite um nome acima e aguarde…</p>
+  {:else}
+    <div class="card">
+      <p>🧙‍♀️ Estimativa para <strong>{data.name}</strong>: <strong>{data.age} anos</strong></p>
+      <progress max="100" value={data.age}></progress>
+    </div>
+  {/if}
 </main>
