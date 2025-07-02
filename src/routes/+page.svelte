33655fc1693d4635e88b787ee8897c33e054abbf
mfelipperd <felipperabelodurans@gmail.com>
2025-07-02 19:59:10 -0300

    feat: create input and logic to fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { goto } from "$app/navigation";
+
+    let nameInput:  string = '';
+    let debounceTimeout: ReturnType<typeof setTimeout>;
+
+    function onInput(): void {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+      const q = nameInput.trim();
+      const currentUrl = new URL(window.location.href);
+    
+      const newUrl = currentUrl.pathname + (currentUrl.search ? `?${currentUrl.searchParams.toString()}` : '');
+      goto(newUrl, {
+        replaceState: true,
+        noScroll: true,
+        keepFocus: true
+      });
+    }, 700);
+  }
+</script>
+<main>
+     <h1>Descubra a idade pelo nome</h1>
+
+  <label for="name">Digite um nome:</label>
+    <input
+    id="name"
+    type="text"
+    bind:value={nameInput}
+    on:input={onInput}
+    placeholder="ex: Ana"
+    autocomplete="off"
+  />
+</main>
